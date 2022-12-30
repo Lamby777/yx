feadb2d4d86940a6c8707a6b6a2440212fafe847
Dex <sheepareawesome777@gmail.com>
2022-12-30 01:26:14 -0500

    Load command line args, show help if none present

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@
 * - Dex
 **/
 
+use std::env;
+use serde::{Serialize, Deserialize};
+
 fn main() {
-	println!("Hello, world!");
+	// Get command line args
+	let args: Vec<String> = env::args().collect();
+	
+	if args.len() < 2 { return show_help(); }
+
+	let cmd = &args[1];		// give the cmd its own binding
+	let args = &args[2..];	// shadow first vec
+}
+
+fn show_help() -> () {
+	println!(include_str!("help.txt"));
 }
