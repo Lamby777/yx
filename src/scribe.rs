d0394c92c952fcd463c895182cbb58ea38e2ffc0
Dex <sheepareawesome777@gmail.com>
2023-04-01 06:30:24 -0400

    Scribe writes new tags to state

--- src/scribe.rs
+++ src/scribe.rs
@@ -3,13 +3,13 @@
 */
 
 use walkdir::WalkDir;
-use crate::classes::ProgramStatePathed;
+use crate::{classes::ProgramStatePathed, sub::add_tags_to, IDFC};
 
 pub enum ScribeMethod<'a> {
 	SplitBy(&'a str),
 }
 
-pub fn import_from_names(st: &mut ProgramStatePathed, method: ScribeMethod) {
+pub fn import_from_names(st: &mut ProgramStatePathed, method: ScribeMethod) -> IDFC<()> {
 	for entry in WalkDir::new(&st.path) {
 		match entry {
 			Err(e)	=> {
@@ -19,9 +19,13 @@ pub fn import_from_names(st: &mut ProgramStatePathed, method: ScribeMethod) {
 			Ok(i)	=> {
 				let fname = i.file_name().to_string_lossy();
 				let tags = process_into_tags(fname.as_ref(), &method);
+
+				add_tags_to(&mut st.state, &st.path, &tags)?;
 			}
 		}
 	}
+
+	Ok(())
 }
 
 fn process_into_tags<'a>(path: &'a str, method: &ScribeMethod) -> Vec<&'a str> {
